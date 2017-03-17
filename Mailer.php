4203dfc08ed16b5665ca578320380b2592ac8fe9
bailangzhan <phpmks@163.com>
2017-03-18 07:50:21 +0800

    publish mailer

---
+++ Mailer.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace bailangzhan;
+
+use Yii;
+use yii\base\InvalidConfigException;
+
+/**
+ * yii2 发送邮件处理类
+ *
+ * For example:
+ *
+ * send a text message
+ * 
+ * ```php
+ *  use bailangzhan\swiftmailer\Mailer;
+ *  $message = [
+        'to' => 'xxx@qq.com',
+        'subject' => 'just a test.',
+        'content' => 'This just a test.',
+    ];
+    $mailer = new Mailer(Mailer::TYPE_1, $message);
+    $result = $mailer->sendMessage();
+ * ```
+ *
+ * If the message you want to send is an html template page, you can do so:
+ * ```php
+ * $message = [
+        'to' => 'xxx@qq.com',
+        'subject' => 'just a test.',
+        'view' => Yii::$app->mailer->viewPath . 'mail-template',
+        'params' => [
+            'name' => 'xiaoming',
+            'age' => 20
+        ]
+    ];
+ * ```
+ * params'name and params'age is the corresponding variable inside the view
+ * 
+ * Send text messages in bulk
+ * ```php
+ * $messages = [
+                    [
+                        'to' => 'xxx@qq.com', 
+                        'subject' => 'just a test.',
+                        'content' => 'This just a test.',
+                    ],
+                    [
+                        'to' => 'xxx@qq.com', 
+                        'subject' => 'just a test2.',
+                        'content' => 'This just a test2.',
+                        'view' => Yii::$app->mailer->viewPath . 'mail-template',
+                        'params' => [
+                            'name' => 'xiaoming',
+                            'age' => 20
+                        ]
+                    ]
+                ];
+    $mailer = new Mailer(Mailer::TYPE_2, $messages);
+    $result = $mailer->sendMessage();
+ *  ```
+ */
+
+class Mailer
+{
+    /**
+     * @var Yii::$app->mailer
+     */
+    private $_mailer;
+
+    /**
+     * @var mailer config, name-value pairs
+     *
+     * For example:
+     *
+     * ```php
+     * [
+     *     'to' => '',
+     *     'subject' => '',
+     *     'content' => '',
+     *     'view' => null,
+     *     'params' => [],
+     * ]
+     * ```
+     */
+    public $message = [];
+    public $type;
+
+    // one
+    const TYPE_1 = 1;
+    // multi
+    const TYPE_2 = 2;
+
+    public function __construct($type, $message = null)
+    {
+        // init
+        if ($this->_mailer == null) {
+            $this->_mailer = Yii::$app->mailer;
+        }
+        $this->message = $message;
+        $this->type = $type;
+
+        // configuration check
+        $this->checkType();
+        $this->check();
+    }
+    /**
+     * send a text message
+     * @param  array   $message  refer to $this->message
+     */
+    protected function text($message = null)
+    {
+        !$message && $message = $this->message;
+        $result = $this->_mailer
+                        ->compose(
+                                !empty($message['view']) ? $message['view'] : null, 
+                                !empty($message['params']) ? (array) $message['params'] : []
+                            )
+                        ->setTo($message['to'])
+                        ->setSubject($message['subject'])
+                        ->setTextBody($message['content']);
+        return $result;
+    }
+    /**
+     * Send text messages in bulk
+     * @return Array $messages
+     */
+    protected function multiText()
+    {
+        $messages = []; 
+        foreach ($this->message as $message) {
+            $messages[] = $this->text($message); 
+        } 
+        return $messages;
+    }
+
+    public function sendMessage()
+    {
+        try {
+            $result = null;
+            // choose diffrent send type
+            switch ($this->type) {
+                case self::TYPE_1:
+                    $result = $this->text()->send();
+                    break;
+                case self::TYPE_2:
+                    $messages = $this->multiText();
+                    $result = $this->sendMultiple($messages);
+                    break;
+                default:
+                    break;
+            } 
+        } catch (\Exception $e) {
+            $msg = $e->getMessage();
+            $logObject = Yii::getLogger();
+            $logObject->log($msg, \yii\log\Logger::LEVEL_ERROR);
+            $logObject->flush(true);
+
+            throw new \Exception("Error: {$msg}.", 1);
+        }
+        
+        return $result;
+    }
+    /**
+     * Check the configuration is correct
+     */
+    protected function check()
+    {
+        $tempMessage = $this->message;
+        $multi = current($tempMessage);
+        $count = 0;
+        switch ($this->type) {
+            case self::TYPE_1:
+                if (!is_array($multi)) {
+                    $this->singleCheck($this->message);
+                    $count ++;
+                }
+                break;
+            case self::TYPE_2:
+                if (is_array($multi)) {
+                    foreach ($this->message as $msg) {
+                        $this->singleCheck($msg);
+                        $count ++;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        if (!$count) {
+            throw new InvalidConfigException("Your configuration is wrong.", 1);
+        }
+        return $count;
+    }
+    protected function singleCheck($message)
+    {
+        if (!is_array($message)) {
+            throw new InvalidConfigException("Mailer::\$message must be an array, please refer to Mailer::\$message configuration.", 1);
+        }
+        if (empty($message['to']) || empty($message['subject'])) {
+            throw new InvalidConfigException("\$message['to'] and \$message['subject'] must be set.", 1);
+        }
+        if (empty($message['view']) && empty($message['content'])) {
+            throw new InvalidConfigException("If you have not set \$message['view'], you have to set \$message['content'].", 1);
+        }
+        return true;
+    }
+    protected function checkType()
+    {
+        if (!in_array($this->type, [self::TYPE_1, self::TYPE_2])) {
+            throw new InvalidConfigException("you have not set up Mailer::\$type, please refer to Mailer::\$type configuration.", 1);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,103 @@
+yii2-swiftmailer
+==========================
+
+此扩展是 Yii2 swiftmailer 邮件类的封装，可直接调用并发送邮件
+
+## 安装
+
+
+推荐使用composer进行安装
+
+```
+$ php composer.phar require bailangzhan/yii2-swiftmailer "@dev"
+```
+
+或者添加
+
+```
+"bailangzhan/yii2-swiftmailer": "@dev"
+```
+
+到你的`composer.json`文件的`require`中
+
+## 使用
+
+### 配置你的mailer组件
+
+
+```
+'mailer' => [
+    'class' => 'yii\swiftmailer\Mailer',
+    'viewPath' => '@common/mail',
+    'useFileTransport' => false,
+    'transport' => [
+        'class' => 'Swift_SmtpTransport',
+        'host' => 'smtp address',
+        'username' => 'your email username',
+        'password' => 'password',
+        'port' => '25',
+        'encryption' => 'tls',
+    ],
+    'messageConfig'=>[  
+       'charset'=>'UTF-8',  
+       'from'=>['your email username' => 'your app name']  
+    ],
+],
+```
+
+### 调用Mailer
+
+#### 发送一封邮件
+
+```php
+use bailangzhan\swiftmailer\Mailer;
+$message = [
+    'to' => '要发送给谁',
+    'subject' => '邮件标题',
+    'content' => '邮件内容',
+];
+$mailer = new Mailer(Mailer::TYPE_1, $messages);
+$result = $mailer->sendMessage();
+```
+
+#### 发送模版邮件
+
+```php
+$message = [
+    'to' => '要发送给谁',
+    'subject' => '邮件标题',
+    'view' => 'mail-template',
+    'params' => [
+        'name' => '白狼栈',
+    ]
+];
+```
+
+发送模版邮件不需要指定content, 但是需要配置模版名, 即view的值，加入你有一个位于 Yii::$app->mailer->viewPath目录下的 mail-template.php 模版，view 填写 mail-template 就好
+
+#### 批量发送邮件
+
+```php
+$messages = [
+	[
+		'to' => '要发送给谁',
+		'subject' => '邮件标题',
+		'content' => '邮件内容',
+	],
+	[
+		'to' => '要发送给谁',
+		'subject' => '邮件标题',
+		'content' => '邮件内容',
+		'view' => 'mail-template',
+		'params' => [
+			'name' => '白狼栈',
+		]
+	]
+];
+$mailer = new Mailer(Mailer::TYPE_2, $messages);
+$result = $mailer->sendMessage();
+```
+
+## 许可
+
+**yii2-swiftmailer** is released under the BSD 3-Clause License. See the bundled `LICENSE.md` for details.
